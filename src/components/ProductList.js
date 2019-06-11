50f582c3618d71c725630f4466060209ee3942c4
FeliOdras <ariane.feldmann@odras.de>
2019-06-11 13:41:16 +0200

    Added render and mapped product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,12 +4,22 @@ export default class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [],
+      products: [{ id: 1, name: "Doom" }, { id: 2, name: "Quake" }],
       cart: []
     };
   }
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Cart</h2>
+        <h2>ProductList</h2>
+        <ul>
+          {this.state.products.map(product => (
+            <li key={product.id}>{product.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
