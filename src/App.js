ca306f9892b609ad0963d1e901d6abaa0561af22
FeliOdras <ariane.feldmann@odras.de>
2019-06-11 14:38:23 +0200

    Created Hook for producyt list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ProductList from "./components/ProductList";
+import ProductListHook from './components/ProductListHook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ProductList />
+        <ProductListHook />
       </header>
     </div>
   );
