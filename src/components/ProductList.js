97ef2cba8c0d84cb66b2a2d05b46053554fc7e2d
FeliOdras <ariane.feldmann@odras.de>
2019-06-11 14:04:36 +0200

    Added Coart output

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,21 +4,36 @@ export default class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [{ id: 1, name: "Doom" }, { id: 2, name: "Quake" }],
+      products: [
+        { id: 1, name: "Doom" },
+        { id: 2, name: "Quake" }],
       cart: []
     };
+  }
+
+  addToCart = (product) => {
+    const newCartItem = { ...product }
+    this.setState(prevState => ({
+      cart: {
+        ...prevState.cart,
+        newCartItem
+      }
+    }))
   }
 
   render() {
     return (
       <div>
         <h2>Cart</h2>
+        {this.state.cart.map(
+          (cartItem, index) => <div key={index}>{cartItem.name}</div>
+        )}
         <h2>ProductList</h2>
-        <ul>
-          {this.state.products.map(product => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
+        {this.state.products.map(product => (
+          <div onClick={() => this.addToCart(product)} key={product.id}>
+            {product.name}
+          </div>
+        ))}
       </div>
     );
   }
